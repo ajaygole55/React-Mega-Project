8c868ad8f38235641ad40d93ebd45b9c2e8ffafe
Ajay <ajayji123gole@gmail.com>
2024-05-29 20:51:31 +0530

    last commit

---
+++ appProject/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://imgs.search.brave.com/EhXaGG8SNDvtBC2QP0lwdMCFfH04c5E1ICG9ZfVAkHo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtcGxhdGZvcm0u/OTlzdGF0aWMuY29t/Ly9uNmExVmx0WVUy/TzZqakRtWTVvUW1S/WlIwWW89LzEyMngx/MjY3OjEwNjd4MjIx/Mi9maXQtaW4vNTAw/eDUwMC85OWRlc2ln/bnMtY29udGVzdHMt/YXR0YWNobWVudHMv/MTM1LzEzNTc4NC9h/dHRhY2htZW50XzEz/NTc4NDgzOA"
+      className='w-20'
+      alt="" />
+    </div>
+  )
+}
+
+export default Logo
